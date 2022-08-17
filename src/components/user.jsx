591fd39eb2bee13c4a2e4345205d5250adb05584
Zhamso Gungaev <zham.drag@gmail.com>
2022-08-17 14:00:51 +0800

    homework Pagination

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Qualitie from "./qualitie";
 import BookMark from "./bookmark";
+import PropTypes from "prop-types";
+
 const User = ({
     _id,
     name,
@@ -10,7 +12,7 @@ const User = ({
     rate,
     onDelete,
     bookmark,
-    onToggleBookMark,
+    onToggleBookMark
 }) => {
     return (
         <tr>
@@ -19,6 +21,7 @@ const User = ({
                 {qualities.map((qual) => (
                     <Qualitie key={qual._id} {...qual} />
                 ))}
+                ;
             </td>
             <td>{profession.name}</td>
             <td>{completedMeetings}</td>
@@ -42,3 +45,14 @@ const User = ({
 };
 
 export default User;
+User.propTypes = {
+    _id: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    qualities: PropTypes.array.isRequired,
+    profession: PropTypes.object.isRequired,
+    completedMeetings: PropTypes.number.isRequired,
+    rate: PropTypes.number.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    bookmark: PropTypes.bool.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired
+};
